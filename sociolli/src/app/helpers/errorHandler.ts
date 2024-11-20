90a1980dfc01db9e8ca3d7b26582e6e4a888e6b9
Shaqila Thurfa <shaqilagunawan20@gmail.com>
2024-11-21 00:35:29 +0700

    feat: errorHandler

--- sociolli/src/app/helpers/errorHandler.ts
+++ sociolli/src/app/helpers/errorHandler.ts
@@ -1,8 +1,23 @@
+import { ZodError } from "zod"
+
 export class HttpError extends Error {
   status: number = 500
   constructor(message: string, status: number) {
     super(message)
     this.status = status
   }
+}
+
+export function errorHandler(error: unknown) {
+  if (error instanceof HttpError) {
+    return Response.json({ message: error.message }, { status: error.status })
+  }
+
+  if (error instanceof ZodError) {
+    const message = error.issues[0].message.toLowerCase()
+    return Response.json({ message }, { status: 400 })
+  }
+
+  return Response.json({ message: "Internal server error" }, { status: 500 })
 }
 
