247e1a19d63e116d95c9ed5fdb8e685d66079e77
Shaqila Thurfa <shaqilagunawan20@gmail.com>
2024-11-22 23:56:47 +0700

    feat: metadata

--- sociolli/src/app/(main)/products/[slug]/page.tsx
+++ sociolli/src/app/(main)/products/[slug]/page.tsx
@@ -2,23 +2,40 @@ import DetailProduct from "@/components/detail product";
 
 import type { SecondArgs } from "@/app/api/products/[slug]/route";
 import type { ProductType } from "@/app/db/models/products";
+import { Metadata } from "next";
+import { getProductByParams } from "@/components/actions";
+
+export async function generateMetadata({ params }: SecondArgs): Promise<Metadata> {
+  const { slug } = params;
+  const product = await getProductByParams(slug);
+
+  // console.log(product);
+  
+
+  return {
+    title: product.name,
+    description: product.description,
+    openGraph: {
+      images: [product.thumbnail]
+    }
+  };
+}
 
 export default async function Page({ params}: SecondArgs) {
 
   const {slug} = params;  
-
   // console.log(slug);
 
-  const response = await fetch(`http://localhost:3000/api/products/${slug}`);
-
-  const products: ProductType[] = await response.json();
+  // const response = await fetch(`http://localhost:3000/api/products/${slug}`);
+  const product = await getProductByParams(slug);
+  // const product: ProductType[] = await response.json();
   
 
   // console.log('ini isi apa',product.price);
   // console.log('ini response',response);
   return (
     <div>
-      <DetailProduct product={products}/>
+      <DetailProduct product={product}/>
     </div>
   )
 }
--- sociolli/src/components/actions.ts
+++ sociolli/src/components/actions.ts
@@ -1,9 +1,21 @@
 "use server"
 
+import { HttpError } from "@/app/helpers/errorHandler";
 import { revalidatePath } from "next/cache"
-
-
+import type { SecondArgs } from "@/app/api/products/[slug]/route";
 
 export async function revalidateByPath(path: string){
   revalidatePath(path)
+}
+
+export async function getProductByParams(slug: string) {
+  const response = await fetch(`http://localhost:3000/api/products/${slug}`);
+  
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new HttpError(errorData.message || "Failed to fetch product", 404);
+  }
+
+  const product = await response.json();
+  return product;
 }
